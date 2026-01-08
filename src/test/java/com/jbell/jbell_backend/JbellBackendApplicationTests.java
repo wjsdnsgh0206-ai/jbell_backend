8a287eadd5e400aedee2b68363a58bda9c9c534e
wjsdnsgh0206@gmail.com <wjsdnsgh0206@gmail.com>
2026-01-08 12:12:25 +0900

    [turtleNeck-JEH] 작업요약 detail: 상세내용

---
+++ src/test/java/com/jbell/jbell_backend/JbellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbell.jbell_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
