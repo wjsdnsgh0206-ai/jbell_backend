0062c351feb778b20be148cc75dc203cce1e971b
j89465137-svg <j89465137@gmail.com>
2026-01-28 12:34:14 +0900

    [turtleNeck-KMJ] 등록, 삭제 성공 detail: 등록, 삭제 성공

--- jbell_backend/src/main/java/jbell/JbellBackendApplication.java
+++ jbell_backend/src/main/java/jbell/JbellBackendApplication.java
@@ -1,8 +1,10 @@
 package jbell;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("jbell.**.mapper")
 @SpringBootApplication
 public class JbellBackendApplication {
 
