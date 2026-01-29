df3c4c9dd2103f9c7404ffca76f0d8e65f2ea36d
wjsdnsgh0206@gmail.com <wjsdnsgh0206@gmail.com>
2026-01-29 17:46:20 +0900

    [turtleNeck-JEH] 기상특보, 재난문자 기능 추가 detail: 기상특보, 재난문자 CRUD 기능 추가

--- jbell_backend/src/main/java/jbell/disaster/dto/PredictionInfoResponse.java
+++ jbell_backend/src/main/java/jbell/disaster/dto/PredictionInfoResponse.java
@@ -1,15 +1,17 @@
 package jbell.disaster.dto;
 
 import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -19,16 +21,29 @@ import lombok.Builder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PredictionInfoResponse {
+//
+//    // 산사태 정보 필드
+//    @JsonProperty("lndslFrcstNm")
+//    private String lndslFrcstNm;
+//
+//    @JsonProperty("prctnInfoAnlssDt")
+//    private String prctnInfoAnlssDt;
+//
+//    @JsonProperty("sgg")
+//    private String sgg;
+	
+	private String visibleYn;
+	
+	// 검색 페이지 이동 필드 
+	private int page = 1;      // 현재 페이지 번호
+    private int limit = 10;    // 페이지당 데이터 수
+    private int offset;        // 시작 지점 (자동 계산용)
 
-    // 산사태 정보 필드
-    @JsonProperty("lndslFrcstNm")
-    private String lndslFrcstNm;
-
-    @JsonProperty("prctnInfoAnlssDt")
-    private String prctnInfoAnlssDt;
-
-    @JsonProperty("sgg")
-    private String sgg;
+    public int getOffset() {
+        return (this.page - 1) * this.limit;
+    }
+	
+	
 
     // 재난문자이력관리 필드
     @JsonProperty("sn") 
@@ -74,6 +89,16 @@ public class PredictionInfoResponse {
         @JsonProperty("MAAS_OBNT_DT")
         private String maasObntDt;
         
+        private String lvl;        // 기상 수준 (주의, 위험 등)
+        
+     // [추가] 검색 및 페이징을 위한 필드
+        private String startDate;  // XML의 #{startDate}와 매핑
+        private String endDate;    // XML의 #{endDate}와 매핑
+        private String keyword;
+     // [추가] 검색 필터용 필드 - XML의 <if test="newsType"> 등과 매치됨
+        private String newsType;  
+        private String region;
+        private String level;
 
    
     
