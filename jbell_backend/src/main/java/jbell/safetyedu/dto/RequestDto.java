0f42670ab5812ef4f545017c3bea54156af6d893
sky-forest-animal <qudwns1216@gamil.com>
2026-01-30 12:15:02 +0900

    [turtleNeck-CBJ] 시민안전교육 주석 수정 detail : 시민안전교육 주석 수정

--- jbell_backend/src/main/java/jbell/safetyedu/dto/RequestDto.java
+++ jbell_backend/src/main/java/jbell/safetyedu/dto/RequestDto.java
@@ -23,27 +23,33 @@ public class RequestDto {
     private String contact;      
     private Boolean isPublic;    
     
-    // [추가] 작성자 ID (입력하지 않으면 서버에서 ADMIN 중 하나 자동 배정)
+    // 작성자 ID (입력하지 않으면 서버에서 ADMIN 중 하나 자동 배정)
     private String userId; 
 
     private List<SectionDto> sections; 
     private List<LinkDto> links;       
 
-    @Getter @Setter @NoArgsConstructor
+    @Getter 
+    @Setter 
+    @NoArgsConstructor
     public static class SectionDto {
         private String id;
         private String subTitle;
         private List<ItemDto> items;
     }
 
-    @Getter @Setter @NoArgsConstructor
+    @Getter 
+    @Setter 
+    @NoArgsConstructor
     public static class ItemDto {
         private String id;
         private String type;
         private String text;
     }
 
-    @Getter @Setter @NoArgsConstructor
+    @Getter 
+    @Setter 
+    @NoArgsConstructor
     public static class LinkDto {
         private String id;
         private String label;
