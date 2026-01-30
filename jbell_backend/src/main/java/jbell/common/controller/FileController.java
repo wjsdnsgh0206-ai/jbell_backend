f13b21504ec53d7632423f7ed01e1277b7e10142
shykimchi <kwjdgns2198@gmail.com>
2026-01-30 14:52:59 +0900

    [turtleNeck-KJH] 파일 저장 및 경로 처리 기능 작업 완료 detail : 파일 저장 및 경로 처리 기능 작업 완료

--- jbell_backend/src/main/java/jbell/common/controller/FileController.java
+++ jbell_backend/src/main/java/jbell/common/controller/FileController.java
@@ -1,23 +1,30 @@
 package jbell.common.controller;
 
-import jbell.common.domain.FileResultVO;
-import jbell.common.response.ApiResponse;
-import jbell.common.service.FileService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import jbell.common.response.ApiResponse;
+import jbell.common.utils.FilesUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/file")
+@Slf4j
 @RequiredArgsConstructor
 public class FileController {
 
-    private final FileService fileService;
+    private final FilesUtils filesUtils;
 
     // 에디터용 이미지 업로드 (단건)
     @PostMapping("/upload/editor")
-    public ApiResponse<FileResultVO> uploadEditorImage(@RequestParam("file") MultipartFile file) {
-        FileResultVO result = fileService.saveFile(file, "INLINE");
-        return ApiResponse.success(result);
+    public ApiResponse<?> uploadEditorImage(@RequestParam("file") MultipartFile file) {
+    	
+    	var fileMetaData = filesUtils.uploadFile(file);
+    	log.info("{}", fileMetaData);
+        return ApiResponse.success(fileMetaData);
     }
 }
--- jbell_backend/src/main/java/jbell/config/WebConfig.java
+++ jbell_backend/src/main/java/jbell/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import jbell.common.interceptor.LogInterceptor;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,11 @@ public class WebConfig  implements WebMvcConfigurer{
 	
 	@Value("${file.upload-dir}")
 	private String uploadDir;
+	
+	@Value("${file.path}")
+	private String fileRealPath;
+	
+
 		
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -28,7 +34,9 @@ public class WebConfig  implements WebMvcConfigurer{
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 	
+	
 	/**
+	 * 주소요청에 따른 외부정적파일 경로 설정
 	 * 정적 리소스(이미지 등) 매핑 설정
 	 */
 	@Override
@@ -37,6 +45,37 @@ public class WebConfig  implements WebMvcConfigurer{
 		registry.addResourceHandler("/uploads/**")
 				// 실제 로컬 컴퓨터의 uploadDir 폴더 내 파일로 연결한다.
 				.addResourceLocations("file:///" + uploadDir);
+		
+		String rootPath = getOSFilePath();
+				
+		registry.addResourceHandler("/api/attachment/**")
+				.addResourceLocations(rootPath + fileRealPath + "/attachment/")
+				.setCachePeriod(3600)
+				.resourceChain(true)
+				.addResolver(new PathResourceResolver());
+		
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+	
+
+	/**
+	 * 운영체제별 경로 설정
+	 * @return
+	 */
+	public String getOSFilePath() {
+		String rootPath = "file:///";
+		String os = System.getProperty("os.name").toLowerCase();
+		
+		if(os.contains("win")) {
+			rootPath = "file:///c:";
+		}else if(os.contains("linux")) {
+			rootPath = "file://";
+		}else if(os.contains("mac")) {			
+			rootPath = "file://";
+		}
+		
+		
+		return rootPath;
 	}
 
 }
